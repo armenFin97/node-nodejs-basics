0702ed7fcdcf0eaf736cbbcd117de4aa363c851d
armenFin97 <armenFin97@gmail.com>
2023-06-20 01:49:58 +0400

    updated task

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
+import 
+const fs = require('fs');
+
+
+
 const create = async () => {
     // Write your code here 
+
+    // create.js - реализовать функцию, которая создает новый файл fresh.txt с содержимым
+    //  I am fresh and youngвнутри files папки(если файл уже существует, Errorсообщение 
+    //     FS operation failedдолжно быть выброшено)
+
+    fs.writeFileSync('./files/resh.txt', 'I am fresh and young', (err) => {
+        err ? console.log(err) : null;
+    })
+
 };
 
 await create();
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,9 @@
+const { log } = require('console');
+
 const rename = async () => {
     // Write your code here 
+
+
 };
 
 await rename();
